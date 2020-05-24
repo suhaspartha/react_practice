a5403ea485b453524d91eb5d0dee649ebc9246a3
suhaspartha <geekystark@gmail.com>
2020-05-24 21:45:22 +0530

    added footer

--- color-palette/src/Palette.js
+++ color-palette/src/Palette.js
@@ -6,28 +6,42 @@ import "./Palette.css";
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 , format: "hex"};
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
     this.changeFormat = this.changeFormat.bind(this);
   }
   changeLevel(level) {
     this.setState({ level });
   }
-  changeFormat(value){
-     this.setState({format: value});
+  changeFormat(value) {
+    this.setState({ format: value });
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.id} level = {level}/>
+      <ColorBox
+        background={color[format]}
+        name={color.id}
+        level={level}
+        key={color.id}
+      />
     ));
     return (
       <div className="Palette">
         {/* {Nav bar here} */}
-        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} defaultFormat="hex"/>
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+          defaultFormat="hex"
+        />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer */}
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
