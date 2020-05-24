7be0b4130947494910cb2fcb7982fbc8ae3d07e1
suhaspartha <geekystark@gmail.com>
2020-05-25 00:48:09 +0530

    added palette route

--- color-palette/src/App.js
+++ color-palette/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import generatePalette from "./ColorHelper";
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
+    console.log(this.findPalette("material-ui-colors"));
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>This is palette list</h1>} />
         <Route
           exact
           path="/palette/:id"
-          render={() => <h1>This is sample palette</h1>}
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
         />
       </Switch>
 
