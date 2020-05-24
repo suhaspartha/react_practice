ea1cd9813bf6c016888c4805b914f9e3d9603e3f
suhaspartha <geekystark@gmail.com>
2020-05-25 01:05:56 +0530

    added palette list and router link

--- color-palette/src/App.js
+++ color-palette/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
+import PaletteList from "./PaletteList";
 import seedColors from "./seedColors";
 import generatePalette from "./ColorHelper";
 
@@ -14,7 +15,7 @@ class App extends Component {
     console.log(this.findPalette("material-ui-colors"));
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>This is palette list</h1>} />
+        <Route exact path="/" render={() => <PaletteList palettes = {seedColors}/>} />
         <Route
           exact
           path="/palette/:id"
---
+++ color-palette/src/PaletteList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class PaletteList extends Component {
+  render() {
+    const { palettes } = this.props;
+    return (
+      <div>
+        {palettes.map((palette) => (
+          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+        ))}
+      </div>
+    );
+  }
+}
+export default PaletteList;
