1f2e60751957442bf4aeb711bcade3b004f3aba1
suhaspartha <geekystark@gmail.com>
2020-05-30 20:04:37 +0530

    make palette list responsive

--- color-palette/src/styles/PaletteListStyles.js
+++ color-palette/src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
   root: {
     backgroundColor: "blue",
@@ -12,6 +13,12 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
+    },
+    [sizes.down("xs")]: {
+      width: "60%",
+    },
   },
   nav: {
     display: "flex",
@@ -19,15 +26,22 @@ export default {
     justifyContent: "space-between",
     alignItems: "center",
     color: "white",
-    "& a":{
-      color: "white"
-    }
+    "& a": {
+      color: "white",
+    },
   },
   palette: {
     boxSizing: "border-box",
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3,30%)",
-    gridGap: "5%",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2,50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1,100%)",
+      gridGap: "1rem",
+    },
   },
 };
--- color-palette/src/styles/sizes.js
+++ color-palette/src/styles/sizes.js
@@ -1,14 +1,13 @@
-export default{
-    up(){
-
-    },
-    down(size){
-        const sizes = {
-            xs: "576px",
-            sm: "768px",
-            md: "992px",
-            lg: "1200px"
-        }
-        return `@media (max-width: ${sizes[size]})`
-    }
-}
+export default {
+  up() {},
+  down(size) {
+    const sizes = {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1600px",
+    };
+    return `@media (max-width: ${sizes[size]})`;
+  },
+};
