7399a7c87bd26da29310e4e524c27ece911cd622
suhaspartha <geekystark@gmail.com>
2020-05-30 20:44:07 +0530

    added animation to palette deletion

--- color-palette/src/PaletteList.js
+++ color-palette/src/PaletteList.js
@@ -3,6 +3,7 @@ import { withStyles } from "@material-ui/core/styles";
 import MiniPalette from "./MiniPalette";
 import { Link } from "react-router-dom";
 import styles from "./styles/PaletteListStyles";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class PaletteList extends Component {
   gotoPalette(id) {
@@ -17,20 +18,22 @@ class PaletteList extends Component {
             <h1 className={classes.heading}>React Colors</h1>
             <Link to="/palette/new">Create Palette</Link>
           </nav>
-          <div className={classes.palette}>
+          <TransitionGroup className={classes.palette}>
             {palettes.map((palette) => (
-              <MiniPalette
-                {...palette}
-                handleClick={() => this.gotoPalette(palette.id)}
-                handleDelete={deletePalette}
-                key={palette.id}
-                id={palette.id}
-              />
+              <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+                <MiniPalette
+                  {...palette}
+                  handleClick={() => this.gotoPalette(palette.id)}
+                  handleDelete={deletePalette}
+                  key={palette.id}
+                  id={palette.id}
+                />
+              </CSSTransition>
               // did not use <Link/> it here bcz styling will be of <a>,
               // recommended not to use hece push to history
               // <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
             ))}
-          </div>
+          </TransitionGroup>
         </div>
       </div>
     );
